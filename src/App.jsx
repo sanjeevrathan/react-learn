3e6a7adda732e91f826370d7640e330a3489fcd9
sanjeevrathan <sanjeevrathan4444@gmail.com>
2024-04-14 14:52:49 +0530

    learning hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,65 @@
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import { useState } from "react";
 
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-
-  const descriptions = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {descriptions} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-    </header>
-  );
-}
-
+import Header from "./components/Header/Header.jsx";
+// import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+
+  const handleSelect = (selectedContent) => {
+    //selectedContent => "component", "jsx", "props", "state"
+    setSelectedTopic(selectedContent);
+    console.log(selectedTopic);
+  };
   return (
     <div>
       <Header />
       <main>
         <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {/* <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image} 
+  />*/}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              components
+            </TabButton>
+            {/* <TabButton label="JSX" /> */}
+            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>state</TabButton>
+          </menu>
+          {selectedTopic}
+        </section>
       </main>
     </div>
   );
